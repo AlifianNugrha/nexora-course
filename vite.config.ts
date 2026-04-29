d4ae60a4b5ab7dbf190114af56aaad6d03d81822
Pandu Alifian <fmenbuilder@gmail.com>
2026-04-29 19:51:28 +0700

    Add @tanstack/start dependency and fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { tanstackRouterVite } from "@tanstack/router-plugin";
-import { tanstackStartVite } from "@tanstack/react-start/vite";
+import { tanstackStartVite } from "@tanstack/start/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
